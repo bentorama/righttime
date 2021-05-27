fbd7a0c8eb284605928ba24b5d20d06b8750c9f4
Edward Bent <bentorama@gmail.com>
2021-05-27 21:30:05 +0100

    Seeded User and Venue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning database..."
+User.destroy_all
+
+puts "creating users..."
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "password"
+  )
+end
+
+puts "users created"
+
+puts "creating venues..."
+
+p @venue_owners = User.first(5)
+
+@venue_owners.each do |owner|
+  5.times do 
+    Venue.create!(
+      address: Faker::Address.street_address,
+      name: Faker::Restaurant.name,
+      description: Faker::Restaurant.description,
+      user: owner
+    )
+end
+
+end
