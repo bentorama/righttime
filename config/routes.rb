a30492a174d529a4cf347b7232b9d3a941e7c411
Ed Sants <edward.sants@googlemail.com>
2021-05-27 20:30:09 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :events
+  resources :venues
+  resources :bookings do
+    resources :reviews, only: [ :new, :create ] 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
