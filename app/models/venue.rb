7ff17775e9e6d147f4a5c4a401810daffb82d841
Ed Sants <edward.sants@googlemail.com>
2021-06-03 15:50:45 +0100

    updated seed files for multi-images

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,7 +3,7 @@ class Venue < ApplicationRecord
   has_many :events, dependent: :destroy
   has_many :bookings, through: :events
   has_many :reviews, through: :bookings
-  has_many_attached :photos
+  has_one_attached :photo
 
   validates :address, :name, :description, presence: true
 end
