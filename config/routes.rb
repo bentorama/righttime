275e9e83c75ecda8953b3d0a594865f376feb54a
Mark Carson <markcarson121@gmail.com>
2021-06-03 22:14:34 +0100

    stripe complete - need to replace ngrok with public access

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :new, :create ] 
   end
 
-  mount StripeEvent::Engine, at: '/event-purchased'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
