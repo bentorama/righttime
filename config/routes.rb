23e64263fefea30d17812a1e90a7c54cb5d6dc73
Mark Carson <markcarson121@gmail.com>
2021-06-03 20:23:26 +0100

    added resources for orders to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  
+  resources :orders, only: [:show, :create]
   resources :events
   resources :venues
   resources :bookings do
