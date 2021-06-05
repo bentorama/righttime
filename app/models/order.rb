1d8766cbde378fcf2359b7e22622d27ca6b107c7
Edward Bent <bentorama@gmail.com>
2021-06-05 14:27:42 +0100

    Combined the bookings and order models

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  has_many :reviews, dependent: :destroy
+  # validates :num_attendees, :total_cost, presence: true, numericality: { greater_than: 0 }
   monetize :amount_cents
 end
