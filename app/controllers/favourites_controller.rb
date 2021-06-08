79bf362131e1140198b393ce24f34050ad7728cc
harriet oughton <harrietoughton@gmail.com>
2021-06-08 21:52:10 +0100

    add to favourites function working

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,30 @@
 class FavouritesController < ApplicationController
+  before_action :set_favourite, only: [:show]
+
+  def show
+  end
+
+  def new
+    @event = Event.find(params[:event_id])
+    @favourite = Favourite.new
+  end
+
+  def create
+    @favourite = Favourite.new
+    @event = Event.find(params[:event_id])
+    @favourite.user = current_user
+    @favourite.event = @event
+    if @favourite.save
+        redirect_to(@favourite)
+    else
+      flash[:alert] = "didn't save!"
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_favourite
+    @favourite = Favourite.find(params[:id])
+  end
 end
