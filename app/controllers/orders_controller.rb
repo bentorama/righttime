9aa3291f29220433e077d90dd84a86afc593066c
Ed Sants <edward.sants@googlemail.com>
2021-06-08 18:46:36 +0100

    add new review

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,7 @@
 class OrdersController < ApplicationController
-  # before_action :find_order, only: [:show, :edit, :update, :destroy]
-
   def create
     event = Event.find(params[:event_id])
-    order = Order.create!(event: event, event_sku: event.sku, amount: event.price_cents, state: 'pending', user: current_user)
+    order  = Order.create!(event: event, event_sku: event.sku, amount: event.price_cents, state: 'pending', user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
@@ -25,4 +23,5 @@ class OrdersController < ApplicationController
   def show
     @order = current_user.orders.find(params[:id])
   end
+
 end
