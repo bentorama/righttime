b5deec3ee110234e0f7031e34bbee26b78a4a96e
Ed Sants <edward.sants@googlemail.com>
2021-06-15 15:23:32 +0100

    add user names to show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,8 @@ end
 
 20.times do
   user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: "password"
   )
