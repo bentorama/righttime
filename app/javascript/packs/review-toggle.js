92847cd80dcbbb2088221aff674a9e83668268a8
Mark Carson <markcarson121@gmail.com>
2021-06-24 19:32:37 +0100

    fixed review toggle bug

--- app/javascript/packs/review-toggle.js
+++ app/javascript/packs/review-toggle.js
@@ -1,5 +1,11 @@
 document.querySelectorAll(".review-click").forEach((button) => { 
   button.addEventListener("click", (e) => {
     e.currentTarget.nextElementSibling.classList.toggle('hide')
+    const revealElement = e.currentTarget.querySelector("#reveal-info");
+    if (revealElement.innerText === "Show Review") {
+      revealElement.innerText = "Hide Review";
+    } else {
+      revealElement.innerText = "Show Review"
+    }
   });
 });
--- app/javascript/packs/switch.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("#reveal-info").forEach((card) => {
-  card.addEventListener("click", (i) => {
-    if (i.currentTarget.innerText === "Show Review") {
-      i.currentTarget.innerText = "Hide Review";
-    } else {
-      i.currentTarget.innerText = "Show Review"
-    }
-  })
-})
