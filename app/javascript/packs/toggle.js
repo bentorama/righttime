b1d3c030fc81c8e607ed6f6c6fb3b2b27f5c5e1d
Edward Bent <bentorama@gmail.com>
2021-07-17 11:37:50 +0100

    Removed toggle.js

--- app/javascript/packs/toggle.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".filter-button").forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.currentTarget.classList.toggle("filter-button-active");
-  });
-});
-
