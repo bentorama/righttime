7764bba1408dc60accbd520cbc3439c56d0be023
Mark Carson <markcarson121@gmail.com>
2021-10-12 16:30:46 +0100

    sign in redirects to previous page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters,
                 if: :devise_controller?
@@ -9,10 +10,22 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || super
   end
 end
